2d2bad5577d54a5e6e9304b52b454a3015114ce8
Max Tybar <maxtybar@amazon.com>
2026-02-17 12:59:56 -0500

    (fix): add KMS encryption for S3 Tables table buckets
    Resolves provider inconsistency error where encryption_configuration
    planned as null but AWS returned AES256 default. Adds dedicated S3
    Tables KMS key with s3tables and maintenance.s3tables service principals,
    and explicit aws:kms encryption on all 5 data lake table buckets.
    
    Closes #38

--- iac/roots/datalakes/equity-price-s3-glue-s3/s3table.tf
+++ iac/roots/datalakes/equity-price-s3-glue-s3/s3table.tf
@@ -1,9 +1,19 @@
 // Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+data "aws_kms_key" "s3tables_kms_key" {
+
+  key_id = "alias/${var.S3_TABLES_KMS_KEY_ALIAS}"
+}
+
 resource "aws_s3tables_table_bucket" "equity_price" {
-  
+
   name = "equity-price-s3-glue-s3table"
+
+  encryption_configuration = {
+    sse_algorithm = "aws:kms"
+    kms_key_arn   = data.aws_kms_key.s3tables_kms_key.arn
+  }
 }
 
 resource "aws_s3tables_namespace" "equity_price" {
@@ -14,11 +24,11 @@ resource "aws_s3tables_namespace" "equity_price" {
 
 module "equity_price" {
 
-  source     = "../../../templates/modules/s3-table-iceberg"
+  source = "../../../templates/modules/s3-table-iceberg"
 
   BUCKET_ARN = aws_s3tables_table_bucket.equity_price.arn
   NAMESPACE  = aws_s3tables_namespace.equity_price.namespace
-  TABLE_NAME  = "equity_price_s3_glue_s3table"
+  TABLE_NAME = "equity_price_s3_glue_s3table"
 
   FIELDS = [
     {
@@ -55,20 +65,20 @@ module "equity_price" {
 }
 
 data "aws_iam_policy_document" "equity_price_bucket_policy_document" {
-  
+
   statement {
-    sid = "AllowAthenaAccess"
+    sid    = "AllowAthenaAccess"
     effect = "Allow"
-    
+
     principals {
       type        = "Service"
       identifiers = ["athena.amazonaws.com"]
     }
-    
+
     actions = [
       "s3tables:*"
     ]
-    
+
     resources = [
       "${aws_s3tables_table_bucket.equity_price.arn}/*",
       aws_s3tables_table_bucket.equity_price.arn
@@ -76,18 +86,18 @@ data "aws_iam_policy_document" "equity_price_bucket_policy_document" {
   }
 
   statement {
-    sid = "AllowGlueAccess"
+    sid    = "AllowGlueAccess"
     effect = "Allow"
-    
+
     principals {
       type        = "Service"
       identifiers = ["glue.amazonaws.com"]
     }
-    
+
     actions = [
       "s3tables:*"
     ]
-    
+
     resources = [
       "${aws_s3tables_table_bucket.equity_price.arn}/*",
       aws_s3tables_table_bucket.equity_price.arn
